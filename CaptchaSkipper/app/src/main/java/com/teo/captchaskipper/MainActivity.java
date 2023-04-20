54b435a57e183a73a0d7894c01a287a3251460e5
Teo <teolands31@gmail.com>
2023-04-20 21:35:56 +0200

    Test captcha skipper

--- CaptchaSkipper/app/src/main/java/com/teo/captchaskipper/MainActivity.java
+++ CaptchaSkipper/app/src/main/java/com/teo/captchaskipper/MainActivity.java
@@ -2,13 +2,29 @@ package com.teo.captchaskipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.start = (Button) findViewById(R.id.start_btn);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(), Web_Wiewer.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
 }
---
+++ CaptchaSkipper/app/src/main/java/com/teo/captchaskipper/Web_Wiewer.java
@@ -0,0 +1,23 @@
+package com.teo.captchaskipper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Web_Wiewer extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_wiewer);
+
+
+
+    }
+}
