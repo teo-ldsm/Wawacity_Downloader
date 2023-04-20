2625a44f55ffa3a7fb4f56a1a803f346f482cafe
Teo <teolands31@gmail.com>
2023-04-20 20:28:30 +0200

    v1.0 (Fonctionnel)

--- config_loader.py
+++ config_loader.py
@@ -1,20 +1,16 @@
 def load() -> dict:
     print("Récupération des données de \'config.txt\'...\n")
     try:
-        file = open("config.txt", "r", encoding="UTF-8")
+        file = open("./config.txt", "r", encoding="UTF-8")
 
     except:
-        print("Le fichier \'config.txt\' est absent. Voulez vous le reconstruire automatiquement ?(Oui/Non)")
 
-        choix_valide = False
-        while not choix_valide:
-            rep = input().upper()
-            if rep in ("OUI", "NON", "N", "O"):
-                choix_valide = True
-            else:
-                print("Réponse invalide. Répondez par oui ou non")
-        build_config()
-        file = open("config.txt", "r", encoding="UTF-8")
+        rep = demande("Le fichier \'config.txt\' est absent. Voulez vous le reconstruire automatiquement ?")
+
+        if rep in ("OUI", "O"):
+            build_config()
+
+        file = open("./config.txt", "r", encoding="UTF-8")
 
     finally:
         config_tmp = []
@@ -35,7 +31,7 @@ def load() -> dict:
         try:
             for i in config_tmp:
                 tmp = i.rsplit("=")
-                config[tmp[0]] = tmp[1]
+                config[tmp[0].strip()] = tmp[1]
 
         except:
 
@@ -62,35 +58,43 @@ def build_config() -> None:
           "Toutes les données de \'config.txt\' vont être effacées. Veuillez les sauvegarder et appuyer sur Entrer")
     input()
     file = open("config.txt", "w", encoding="UTF-8")
-    file.write("# Les lignes qui commencent par \"#\" ne sont pas prises en compte \n\
-                            # Veuillez ne pas laisser de champs vide sans un \"#\" en début de ligne \n\
-                            # Les champs seront remplis automatiquement si non précisé ici \n\
-                            # - PATH : Les medias seront téléchargés dans ce dossier \n\
-                            # - QUALITY : Qualité par défaut pour télécharger les médias \n\
-                            # - SITE : Site par défaut ou télécharger les médias \n\
-                            # PATH= \n\
-                            # QUALITY= \n\
-                            # SITE= \n\n\
-                            # Retirez les # si vous utilisez un serveur plex et que vous souhaitez l'actualiser \
-                                                                                    après chaque téléchargement \n\
-                            # SERVER_IP= \n\
-                            # PORT=32400 \n\
-                            # TOKEN= ")
+    file.write("# Les lignes qui commencent par \"#\" ne sont pas prises en compte \n"
+               "# Veuillez ne pas laisser de champs vide sans un \"#\" en début de ligne \n"
+               "# Les champs seront remplis automatiquement si non précisé ici \n"
+               "# - PATH : Les medias seront téléchargés dans ce dossier \n"
+               "# - QUALITY : Qualité par défaut pour télécharger les médias \n"
+               "# - SITE : Site par défaut ou télécharger les médias \n"
+               "#PATH=\n"
+               "#QUALITY=\n"
+               "#SITE=\n\n"
+               "# Retirez les # si vous utilisez un serveur plex et que vous souhaitez l'actualiser "
+               "après chaque téléchargement \n"
+               "#SERVER_IP=\n"
+               "PORT=32400\n"
+               "#TOKEN=")
 
-    print("config.txt à été reconstruit avec succès !\n")
     file.close()
 
+    print("config.txt à été reconstruit avec succès !\n")
 
-def fill_config(path=False, quality=False, site=False, plex=False) -> None:
+    rep = demande("Voulez vous ouvrir le bloc-note pour remplir les valeurs du fichier maintenant ?")
+
+    if rep in ("OUI", "O"):
+        fill_config(tous=True)
+        verify_config()
+
+
+def fill_config(tous=False, path=False, quality=False, site=False, plex=False) -> None:
     config = load()
-    file = open("config.txt", "r", encoding="UTF-8")
+    if tous:
+        path, quality, site, plex = True, True, True, True
     args = {"PATH": path, "QUALITY": quality, "SITE": site, "SERVER_IP": plex, "PORT": plex, "TOKEN": plex}
     for i in args:
         if args[i]:
             if i in config:
                 print(f"La valeur {i} est par défaut a {config[i]}. Voulez vous la modifier ?(OUI/NON)")
             else:
-                print(f"La valeur {i} n'a pas été renseigné. Voulez vous la renseigner ?(OUI/NON")
+                print(f"La valeur {i} n'a pas été renseigné. Voulez vous la renseigner ?(OUI/NON)")
 
             choix_valide = False
             rep = None
@@ -105,12 +109,25 @@ def fill_config(path=False, quality=False, site=False, plex=False) -> None:
                 print(f"Entrez la nouvelle valeur de {i}")
                 rep = input()
 
-                with open("config.txt", "r", encoding="UTF-8") as file:
+                with open("./config.txt", "r", encoding="UTF-8") as file:
                     lignes = file.readlines()
 
-                for nb_ligne in range(len(lignes)):
-                    if i+"=" in lignes[nb_ligne]:
-                        lignes[nb_ligne] = f"{i}={rep}"
+                for nb_ligne in range(len(lignes)): # TODO Ici c beugué les lignes se chevauchent quand tu les créé
+                    if i + "=" in lignes[nb_ligne]:
+                        lignes[nb_ligne] = f"{i}={rep}\n"
 
-                with open("config.txt", "w", encoding="UTF-8") as file:
+                with open("./config.txt", "w", encoding="UTF-8") as file:
                     file.writelines(lignes)
+
+
+def demande(msg: str = ""):
+    choix_valide = False
+    rep = None
+    while not choix_valide:
+        rep = input(f"{msg} (Oui/Non)").upper()
+        if rep in ("OUI", "NON", "N", "O"):
+            choix_valide = True
+        else:
+            print("Réponse invalide. Répondez par oui ou non")
+
+    return rep
