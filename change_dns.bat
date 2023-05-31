88c49e877aedcbd75215f74388301c3f35800519
Teo <teolands31@gmail.com>
2023-05-31 08:26:22 +0200

    Ajout dns ipv6

--- change_dns.bat
+++ change_dns.bat
@@ -1,2 +1,4 @@
 netsh interface ip set dns name="%1" static 8.8.8.8
 netsh interface ipv4 add dnsservers "%1" 8.8.4.4 index=2
+netsh interface ipv6 set dns name="%1" static 2001:4860:4860::8888
+netsh interface ipv6 add dnsservers "%1" 2001:4860:4860::8844 index=2
