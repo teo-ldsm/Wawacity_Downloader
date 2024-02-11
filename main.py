6bc8a9ced8e322b2578ed70b50afedf30d53b4bd
Teo <teolands31@gmail.com>
2024-02-11 16:26:21 +0100

    Changement numéro de version

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ import wget
 from recup_lien_1fichier import *
 
 
-version = "v1.1.1-beta"     # TODO Modifier le numéro de version
+version = "v1.1.2-beta"     # TODO Modifier le numéro de version
 
 if os.name == 'nt':  # Windows
     os.system('cls')
